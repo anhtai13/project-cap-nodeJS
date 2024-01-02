ea684a6cc1acc6d1be06f08dfff12d9dfa62608f
lnbkhuong <lengobaokhuong1@gmail.com>
2024-01-02 18:53:19 +0700

    update user

--- src/application/services/user.service.js
+++ src/application/services/user.service.js
@@ -1,97 +1,101 @@
-import { validateEmail, validateIdUserDetails, validatePassword, validateRoleUser, validateUserName } from "../../utils/validationUser.js";
+import {
+  validateEmail,
+  validateIdUserDetails,
+  validatePassword,
+  validateRoleUser,
+  validateUserName,
+} from "../../utils/validationUser.js";
 import userRepositories from "./../repositories/user.repositories.js";
 
 // Hàm tìm kiếm người dùng
 const searchUsers = (params, callback) => {
-    userRepositories.searchUsers(params, (err, result) => {
-        if (err) {
-            callback(err, null);
-        } else {
-            callback(null, result);
-        }
-    });
+  userRepositories.searchUsers(params, (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, result);
+    }
+  });
 };
 
 // Hàm thêm người dùng mới
 const addUser = (params, callback) => {
-    if (!params.username && !params.password && !params.email && !params.first_name && !params.last_name) {
-        callback({ message: "Please complete all information" }, null)
-    }
-    else if (!validateEmail(params.email)) {
-        callback({ message: "Invalid email!" }, null);
-    } else if (!validatePassword(params.password)) {
-        callback({ message: "Invalid password!" }, null);
-    } else if (!validateUserName(params.username)) {
-        callback({ message: "Invalid username!" }, null);
-    } else if (!validateRoleUser(params.role)) {
-        callback({ message: "Invalid role!" }, null);
-    } else {
-        userRepositories.addUser(params, (err, result) => {
-            if (err) {
-                callback(err, null);
-            } else {
-                callback(null, result);
-            }
-        });
-    }
+  if (
+    !params.username &&
+    !params.password &&
+    !params.email &&
+    !params.first_name &&
+    !params.last_name
+  ) {
+    callback({ message: "Please complete all information" }, null);
+  } else if (!validateEmail(params.email)) {
+    callback({ message: "Invalid email!" }, null);
+  } else if (!validatePassword(params.password)) {
+    callback({ message: "Invalid password!" }, null);
+  } else if (!validateUserName(params.username)) {
+    callback({ message: "Invalid username!" }, null);
+  } else if (!validateRoleUser(params.role)) {
+    callback({ message: "Invalid role!" }, null);
+  } else {
+    userRepositories.addUser(params, (err, result) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, result);
+      }
+    });
+  }
 };
 
 // Hàm lấy chi tiết người dùng
 const getDetailUser = (params, callback) => {
-    if (!validateIdUserDetails(params.id)) {
-        callback({ message: "Invalid ID!" }, null);
-    } else {
-        userRepositories.getDetailUser(params, (err, result) => {
-            if (err) {
-                callback(err, null);
-            } else {
-                callback(null, result);
-            }
-        });
-    }
+  if (!validateIdUserDetails(params.id)) {
+    callback({ message: "Invalid ID!" }, null);
+  } else {
+    userRepositories.getDetailUser(params, (err, result) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, result);
+      }
+    });
+  }
 };
 
 // Hàm cập nhật thông tin người dùng
 const updateUser = (params, callback) => {
-    if (!params.username && !params.password && !params.email && !params.first_name && !params.last_name) {
-        callback({ message: "Please fill in all the information" }, null)
-    } else if (!validatePassword(params.password)) {
-        callback({ message: "Invalid password!" }, null);
-    } else if (!validateEmail(params.email)) {
-        callback({ data: "email", message: "Invalid email!" }, null);
-    } else if (!validateUserName(params.username)) {
-        callback({ data: "username", message: "Invalid username!" }, null);
-    } else {
-        userRepositories.updateUser(params, (err, result) => {
-            if (err) {
-                callback(err, null);
-            } else {
-                callback(null, result);
-            }
-        });
-    }
+  if (!validatePassword(params.password)) {
+    callback({ message: "Invalid password!" }, null);
+  } else {
+    userRepositories.updateUser(params, (err, result) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, result);
+      }
+    });
+  }
 };
-
 
 // Hàm xóa người dùng
 const deleteUser = (params, callback) => {
-    if (!validateIdUserDetails(params.id)) {
-        callback({ message: "Invalid ID!" }, null);
-    } else {
-        userRepositories.deleteUser(params, (err, result) => {
-            if (err) {
-                callback(err, null);
-            } else {
-                callback(null, result);
-            }
-        });
-    }
+  if (!validateIdUserDetails(params.id)) {
+    callback({ message: "Invalid ID!" }, null);
+  } else {
+    userRepositories.deleteUser(params, (err, result) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, result);
+      }
+    });
+  }
 };
 
 export default {
-    searchUsers,
-    addUser,
-    getDetailUser,
-    updateUser,
-    deleteUser
+  searchUsers,
+  addUser,
+  getDetailUser,
+  updateUser,
+  deleteUser,
 };
